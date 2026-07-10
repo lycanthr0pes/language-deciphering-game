506d57dcb1f1f7dde9546185a26e16221c583a56
Takuma Tateishi <takutate.0427@gmail.com>
2026-07-10 15:29:40 +0900

    main: comment out basePath and output settings in next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/',
-   output: "export",
+  // basePath: '/',
+  // output: "export",
 };
 
 export default nextConfig;
